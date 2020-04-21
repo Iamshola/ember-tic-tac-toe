89e1d45815dee7e0fba9238cb69dec36b3e66d8e
Adesola Oni-Shogbonyo <adesola.oni-shogbony@MLH-0379.smart.lan>
2020-04-21 19:52:29 +0100

    users are no longer able to click the same box

--- app/controllers/game-board.js
+++ app/controllers/game-board.js
@@ -4,49 +4,67 @@ import { tracked } from '@glimmer/tracking';
 
 export default class GameBoardController extends Controller {
 
-  @tracked total = false;
+  @tracked playerTurn = false;
 
   constructor() {
     super(...arguments);
     this.playerX = []
     this.playerO = []
+    this.selectedSquare = []
     this.winner = ''
     this.winningSolution = [
-    ['1', '2', '3'], ['4', '5', '6'],
-    ['7', '8', '9'], ['1', '4', '7'],
-    ['2', '5', '8'], ['3', '6', '9'],
-    ['1', '5', '9'], ['3', '5', '7']
-  ]
- }
+      ['1', '2', '3'], ['4', '5', '6'],
+      ['7', '8', '9'], ['1', '4', '7'],
+      ['2', '5', '8'], ['3', '6', '9'],
+      ['1', '5', '9'], ['3', '5', '7']
+    ]
+  }
 
-  winLogic(){
+  winLogic() {
     return this.winner == 'X' ? alert('the winner is O') : alert('the winner is X')
   }
 
-  @action handleClick(){
-
-   this.total = !this.total
- 
-   if(this.total == false){
-     this.playerO.push(event.target.id)
-     event.target.innerHTML = this.total === 'false' ? 'O' : 'X'
-   } else{
-     this.playerX.push(event.target.id)
-     event.target.innerHTML = this.total === 'true' ? 'X' : 'O'
-   }
-
-    if (this.winningSolution.map(x => x.every(v => this.playerO.includes(v))).includes(true) && this.playerO.length == 3) {
+  identfyWinner() {
+    if (this.winningSolution.map(x => x.every(v => this.playerO.includes(v))).includes(true) && this.playerO.length >= 3) {
       this.winner = 'O'
       this.playerO = []
-      setTimeout(this.winLogic, 300)
-  
+      this.winLogic()
     }
-    if (this.winningSolution.map(x => x.every(v => this.playerX.includes(v))).includes(true) && this.playerX.length == 3) {
+    if (this.winningSolution.map(x => x.every(v => this.playerX.includes(v))).includes(true) && this.playerX.length >= 3) {
       this.winner = 'X'
       this.playerX = []
-      setTimeout(this.winLogic, 300)
+      this.winLogic()
     }
   }
+
+  @action handleClick() {
+
+    this.playerTurn = this.clickedBefore == true ? this.playerTurn : !this.playerTurn
+    this.selectedSquare.push(event.target.id)
+
+    if (this.playerTurn == false) {
+      if (this.selectedSquare.length > 1 && this.selectedSquare.filter(x => x == event.target.id).length == 2) {
+        this.clickedBefore = true
+        this.selectedSquare.pop()
+      } else {
+        this.playerO.push(event.target.id)
+        event.target.innerHTML = this.playerTurn === 'false' ? 'O' : 'X'
+        this.clickedBefore = false
+      }
+
+    } else {
+      if (this.selectedSquare.length > 1 && this.selectedSquare.filter(x => x == event.target.id).length == 2) {
+        this.clickedBefore = true
+        this.selectedSquare.pop()
+      } else {
+        this.playerX.push(event.target.id)
+        event.target.innerHTML = this.playerTurn === 'true' ? 'X' : 'O'
+        this.clickedBefore = false
+      }
+    }
+    this.identfyWinner()
+  }
+
 }
 
 
